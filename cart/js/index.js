5b68c70edfef17b893678639d1138a2e9852504b
xinghe1997 <57445290+xinghe1997@users.noreply.github.com>
2020-10-13 08:45:24 +0800

    创建

---
+++ cart/js/index.js
@@ -0,0 +1,9 @@
+$(function(){
+	//初始化方法
+	$(".container").fullpage({
+		sectionsColor: ["#fadd67", "#84a2d4", "#ef674d", "#ffeedd", "#d04759", "#84d9ed", "#8ac060"],
+		navigation: true,
+		verticalCentered: false,
+	})
+	
+});
